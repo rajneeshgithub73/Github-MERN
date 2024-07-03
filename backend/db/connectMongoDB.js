7dc6914af4ca0fa3aa2af4361baf19e551f7506f
rajneeshgithub73 <rajneeshrj73@gmail.com>
2024-07-03 17:04:52 +0530

    project update

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connectMongoDB() {
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect(`${process.env.MONGO_URI}/githubmern`);
 		console.log("MONGODB connected");
 	} catch (error) {
 		console.log("Error connecting to MongoDB: ", error.message);
